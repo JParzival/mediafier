7fd2eee6e60d4f45aee0bf26f9271d33307dc1ab
Jorge <jorge.deandres@hotmail.es>
2021-06-04 17:40:57 +0200

    Updated __init__,py to make a package

---
+++ mediafier/__init__.py
@@ -0,0 +1,7 @@
+"""Imports for package mediafier"""
+from __future__ import absolute_import
+
+import mediafier.image as image
+import mediafier.video as video
+
+__version__ = '0.1.1'
---
+++ mediafier/image/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from mediafier.image.checks import *
+from mediafier.image.color import *
+from mediafier.image.cropping import *
+from mediafier.image.draw import *
+from mediafier.image.modifications import *
+from mediafier.image.orientation import *
+from mediafier.image.size import *
+
+__version__ = '0.1.1'
