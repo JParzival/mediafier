3175053b654d7838a43aebc4de2acf2ce6c72803
Alba Gállego <84712827+albagallegom@users.noreply.github.com>
2021-06-06 11:22:12 +0200

    Update orientation.py
    Function "rotateTextBased" included

--- mediafier/image/orientation.py
+++ mediafier/image/orientation.py
@@ -1,5 +1,7 @@
 import cv2
 import imutils
+import pytesseract
+import re
 
 from ..utils.utils import intdetector, stringdetector, str2bool
 
@@ -89,3 +91,52 @@ def rotate(img, degrees=0, force_fit=False):
             return cv2.warpAffine(img, M, (w, h))
         else:
             return imutils.rotate_bound(img, degrees)
+        
+def rotateTextBased (img, pytesseract_path = "C:/Program Files/Tesseract-OCR/tesseract.exe", contrast = False):
+    
+    """
+    This function retrieves an image rotated by the angle that allows to read the text properly.
+    The behaviour is based on pytesseract OCR, that has to be installed on your PC. You can download it from: 
+    https://github.com/UB-Mannheim/tesseract/wiki
+
+    Args:
+        img (:obj: array, mandatory): 
+            Image to rotate.
+        pytesseract_path (:obj: string, optional): 
+            Path in which pytesseract OCR is installed. By default it takes the most common path.
+        contrast (:obj: boolean, optional): 
+            If true, the dark elements of the image will be highlighted, reducing the noise of the image.
+            The contrast won't be applied in the returned image
+
+    Returns:
+        :obj: array:
+            The resulting object is the image, in the same format as inputted, but with the rotation applied.
+
+    Raises:
+        ValueError: Raised if contrast parameter is not boolean.
+        ValueError: Raised if the image has not the proper format.
+        ValueError: Raised if no tesseract OCR is found.
+    """
+    pytesseract.pytesseract.tesseract_cmd = pytesseract_path
+
+    try:
+        img_angle = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    except: 
+        raise ValueError("The input image has not the correct format. Array expected, got %s." % (type(img)))
+
+    if(contrast == True):
+        black = 100
+        img_angle = cv2.threshold(img_angle, black, 255, cv2.THRESH_BINARY)[1]
+    elif(contrast != False):
+        ValueError("Expected contrast parameter to be boolean, got %s." % (type(contrast)))
+
+    try:
+        ocr_result = pytesseract.image_to_osd(img_angle)
+    except:
+        raise ValueError("No tesseract OCR has been detected. \
+            Please, ensure you have it installed and that the installation path is reflected in 'pytesseract_path' parameter. \
+                You can download it from https://github.com/UB-Mannheim/tesseract/wiki.")
+
+    angle = int(re.search('(?<=Rotate: )\d+', ocr_result).group(0))
+
+    return imutils.rotate_bound(img, angle)
